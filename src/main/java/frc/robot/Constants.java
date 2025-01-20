aff4a2255d114039e08d279ffd94e0a687c0c2e4
Abhinav dashora <abhinavdashora@Abhinavs-Air.hsd1.mn.comcast.net>
2025-01-20 15:32:06 -0600

    first 3 methods

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,11 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.math.util.Units;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -27,36 +32,216 @@ public final class Constants {
     }
     public static class mod1{
 
-
+      public static final int drive_ID = 0;
+      public static final int turn_ID = 0;
       
     }
     public static class mod2{
 
+      public static final int drive_ID = 0;
+      public static final int turn_ID = 0;
 
       
     }
     public static class mod3{
 
 
-      
+      public static final int drive_ID = 0;
+      public static final int turn_ID = 0;
     }
 
   }
 
-  public static class PID{
+  
 
       public static final double Turn_KP = 0.0;
       public static final double Turn_KI = 0.0;
       public static final double Turn_KD = 0.0;
 
-      public static final double Drive_KP = 0.0;
-      public static final double Drive_KI = 0.0;
-      public static final double Drive_KD = 0.0;
+      public static final double Drive_KP = 0.1;
+      public static final double Drive_KI = 0.2;
+      public static final double Drive_KD = 0.3;
 
       public static final double Gyro_offset = 0.0;
+
+      public static final double driveMotorRatio = 0;
+      public static final double turningMotorRatio =0 / 7;
+    
+      //Conversion Factors
+      public static final double driveMotorPosFactor = Math.PI * driveMotorRatio * 0;
+      public static final double driveMotorVelFactor = driveMotorPosFactor / 60;
+      public static final double turningMotorPosFactor = 360 / turningMotorRatio;
+    
+      
+      //Max drive of the robot (in meters per second or radians per second)
+      public static final double maxSpeed = 5;
+      public static final double maxAutoSpeed = 2.5;
+      public static final double maxAutoAcceleration = 0.5;
+      public static final double maxAngularVelocity = 7.0;
+      public static final double maxAutoAngularVelocity = Math.PI;
+
+      public static final double robotLength = Units.inchesToMeters(23);
+      public static final double robotWidth = Units.inchesToMeters(23);
+
+    
+  
+
+  public static final SwerveDriveKinematics SwerveMap = new SwerveDriveKinematics(
+    new Translation2d(robotLength / 2, robotWidth / 2), //++
+    new Translation2d(robotLength / 2, -robotWidth /2),  //+-
+    new Translation2d(-robotLength / 2, robotWidth / 2), //-+
+    new Translation2d(-robotLength / 2, -robotWidth / 2) //--
+
+    );
+
+  
+
+    //Intake Constants
+
+    public static final int intakeMotorID = 44;
+
+    public static final double intakeMotorSpeed = 1;
+
+        //Pivot Angles -3
+
+        public static final double intakePos = 73; //102 original
+        public static final double subwooferPos = intakePos+15.5;
+        public static final double ampPos = intakePos+39;
+        public static final double podiumPos = intakePos+8.5;
+        public static final double sourcePos = intakePos+10;
+        public static final double backshotPos = intakePos+39.75;
+
+
+        //test values
+        // public static final double intakePos = 103.5;
+        // public static final double subwooferPos = 118;
+        // public static final double ampPos = 141.5;
+        // public static final double podiumPos = 114.5;
+        // public static final double sourcePos = 113;
+        // public static final double backshotPos = 142.5;
+
+
+    //Shooter Constants
+
+    public static final double alignSpeed = 0.25;
+
+    public static final int fastShooterMotorID = 41;
+    public static final int slowShooterMotorID = 35;
+    public static final int shooterHoldMotorID = 38;
+
+    public static final int shooterPotID = 1;
+
+
+    public static final double shooterIntakeSpeed = 0.5;
+
+    public static final double shooterHighSpeed = 0.8;
+    public static final double shooterLowSpeed = 0.2;
+    public static final double shooterBumpSpeed = 0.2;
+
+    public static final double shooterTargetVelocity = -4400;
+
+
+    //Vision Constants
+
+    public static final double cameraFOV = 42; //camera FOV in degrees
+    public static final double cameraXRez = 640; //camera horizontal resolution in pixels
+    public static final double cameraFOVRatio = cameraXRez / cameraFOV; //camera FOV in pixels
+
+    public static final double alignkP = 0.2;
+
+    public static final int speakerTagID = 1;
+
+
+
+     
+
+
+    //Pivot Constants
+
+    public static final int pivotMotorID = 47;
+
+    //test values
+    // public static final double pivotkP = 1.5;
+    // public static final double pivotkI = .2;
+    // public static final double pivotkD = 0.6;
+
+  //match values
+
+    // public static final double pivotkP = .007; 
+    // public static final double pivotkI = 0;
+    // public static final double pivotkD = 0.002;
+
+    public static final double pivotkP = 1.1; 
+    public static final double pivotkI = 0.2;
+    public static final double pivotkD = 0.05;
+
+
+    public static final double pivotZeroValue = 177.2;
+    public static final double pivotNinetyValue = 133.7;
+
+    public static final double pivotPotToDegrees = (177.2-133.7)/90;
+
+
+
+
+    //Climber Constants
+
+    public static final int climberMotorLeftID = 40;
+    public static final int climberMotorRightID = 29;
+
+    public static final double climberUpSpeed = 0.5;
+    public static final double climberSlowSpeed = 0.5;
+    public static final double climberDownSpeed = 0.5;
+
+    //
+
+    //public static final int climberLeftServoID = 0;
+    //public static final int climberRightServoID = 1;
+
+
+  //auto constants
+  //change PID for testing swerve auto
+
+    public static final double autoRotateP = 0.25;
+    public static final double autoRotateI = 0;
+    public static final double autoRotateD = 0;
+
+    public static final double autoTurningP = 0.5;
+    public static final double autoTurningI = 0;
+    public static final double autoTurningD = 0;
+
+    public static final double autoXP = 0.5;
+    public static final double autoXI = 0;
+    public static final double autoXD = 0;
+
+    public static final double autoYP = 0.5;
+    public static final double autoYI = 0;
+    public static final double autoYD = 0;
+
+    public static final TrapezoidProfile.Constraints autoTurnController = new TrapezoidProfile.Constraints(maxAutoAngularVelocity, maxAutoAngularVelocity);
+
+    //feedforward constants
+  public static final double ffkS = 0.99;
+  public static final double ffkV = 0.5; //5 m/s 2.44
+  public static final double ffkA = 0.1; //accel to 5 m/s 0.27
+
+
+
+  public static class vision {
+    //limelight FOV 80x54
+    //x
+    public static final double width = 80;
+    //y
+    public static final double height = 54;
+    //max voltage for horizontal allignment
+    public static final double max_speed = maxSpeed / 4;
+    public static final double kp = 2 / width * max_speed;
+    public static final double ki = 0;
+    public static final double kd = 0;
   }
 
 
-
-
 }
+
+
+
--- src/main/java/frc/robot/subsystems/Swerve_Module.java
+++ src/main/java/frc/robot/subsystems/Swerve_Module.java
@@ -2,7 +2,142 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.subsystems;
+
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.filter.LinearFilter;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import frc.robot.Constants;
 
 /** Add your docs here. */
-public class Swerve_Module {}
+public class Swerve_Module {
+private final SparkMax driveMotor; //creating motors
+private final SparkMax turnMotor;
+
+private final SparkMaxConfig drive_config; //creating configs
+private final SparkMaxConfig turn_config;
+
+private final int module_order;
+private final int module_number;
+
+private final PIDController turn_controller; //creating PID controllers
+private final PIDController drive_Controller;
+
+private final LinearFilter drive_VelocityFilter = LinearFilter.singlePoleIIR(0,0);
+private final LinearFilter desired_drive_Filter = LinearFilter.singlePoleIIR(0,0);
+
+private final RelativeEncoder Drive_encoder; //creating encoders
+private final RelativeEncoder Turn_encoder;
+
+private final CANcoder best_turn;
+
+private final Rotation2d Swerve_offset; //offset
+
+
+Swerve_Module(int module_number, int module_order, int drive_ID, int turn_ID, Rotation2d Swerve_offset, int CanCoderID){//add more
+
+     this.module_number = module_number;
+     this.module_order = module_order;
+     this.Swerve_offset = Swerve_offset;
+
+     this.best_turn = new CANcoder(CanCoderID);
+     this.driveMotor = new SparkMax(drive_ID, MotorType.kBrushless);
+
+     Drive_encoder = driveMotor.getEncoder();
+
+    this.drive_config      //configguring the motors
+    .inverted(false) 
+    .idleMode(IdleMode.kBrake)
+    .smartCurrentLimit(0)
+    .voltageCompensation(0);
+    
+    this.drive_config.encoder
+    .positionConversionFactor(0)
+    .velocityConversionFactor(0);
+
+    this.Drive_encoder.setPosition(0);
+
+    this.driveMotor.configure(drive_config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    
+    this.drive_Controller = new PIDController(Constants.Drive_KP, Constants.Drive_KI, Constants.Drive_KD);
+
+
+
+
+   
+    this.turnMotor = new SparkMax(turn_ID, MotorType.kBrushless);
+
+    Turn_encoder = turnMotor.getEncoder();
+
+    this.turn_config
+    .inverted(false)
+    .idleMode(IdleMode.kBrake)
+    .smartCurrentLimit(0)
+    .voltageCompensation(0);
+
+    this.turn_config.encoder
+    .positionConversionFactor(0)
+    .velocityConversionFactor(0);
+
+    this.Turn_encoder.setPosition(0);
+
+    this.turnMotor.configure(turn_config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+
+    
+   
+
+
+    this.turn_controller = new PIDController(Constants.Turn_KP, Constants.Turn_KI, Constants.Turn_KD); //PID turn controller
+
+}
+
+    public double get_turn_motor_volt(){
+        return this.turnMotor.getBusVoltage();
+
+}
+public SwerveModuleState getState(){
+
+    return SwerveModuleState(this.Drive_encoder.getVelocity(), this.getCANCoder());
+}
+
+public SwerveModulePosition getposition (){
+
+    return new SwerveModulePosition(this.Drive_encoder.getPosition(), new Rotation2d(Turn_encoder.getPosition()));
+}
+
+public Rotation2d getCANCoder() {
+    return Rotation2d.fromDegrees(this.best_turn.getAbsolutePosition());
+    //gets CANCoder position in degrees
+}
+
+public void resetToAbsolute (){
+
+    System.out.print(getCANCoder().getDegrees());
+
+    this.Turn_encoder.setPosition(Swerve_offset.getDegrees());
+    //sets turning encoders to abosolute position
+}
+
+
+
+
+
+
+
+
+}
+
+
+
